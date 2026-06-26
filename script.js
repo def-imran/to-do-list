a44c021f4a37a3c62237df09c11c4d02fcf12b36
ImranElOuafi <imran.el.ouafi@hva.nl>
2026-06-26 16:09:08 +0200

    add initial implementation of addTask function to handle task input

---
+++ script.js
@@ -0,0 +1,8 @@
+function addTask(){
+    const taskInput = document.getElementById("task").value;
+    const taskButton = document.querySelector(".add-button");
+
+    const taskDisplay = document.querySelector(".display-task");
+
+    
+}
