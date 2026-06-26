56d0d12409f45a8b2feba143041544c84e19f7d5
ImranElOuafi <imran.el.ouafi@hva.nl>
2026-06-26 19:04:52 +0200

    update task display functionality and enhance styles for task items

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-function addTask(){
-    const inputElement = document.getElementById("task");
-    const taskInput = inputElement.value;
-    inputElement.value="";
-    const taskButton = document.querySelector(".add-button");
+tasks = [];
 
-    const taskDisplay = document.querySelector(".display-task");
+function addTask() {
+  const inputElement = document.getElementById("task");
+  const taskInput = inputElement.value;
+  inputElement.value = "";
+  const taskButton = document.querySelector(".add-button");
 
-    tasks = [];
+  const taskDisplay = document.querySelector(".display-task");
 
-    tasks.push(taskInput);
 
-    
-    console.log(tasks)
+  tasks.push(taskInput);
+  taskDisplay.innerHTML=``
+
+  tasks.forEach((task) => {
+    taskDisplay.innerHTML += `
+         <div class="display-task">
+          <div class="display-div">
+            <input type="checkbox" name="task">
+            <label for="task">${task}</label>
+          </div>
+        </div>
+        `
+  })
+
+
+
+  console.log(tasks)
+
 }
+
