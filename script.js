48039c6ba48f47bb7968ed240ffff1339390856e
ImranElOuafi <imran.el.ouafi@hva.nl>
2026-06-26 16:56:40 +0200

    implement task addition functionality and update styles for task input

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function addTask(){
-    const taskInput = document.getElementById("task").value;
+    const inputElement = document.getElementById("task");
+    const taskInput = inputElement.value;
+    inputElement.value="";
     const taskButton = document.querySelector(".add-button");
 
     const taskDisplay = document.querySelector(".display-task");
 
+    tasks = [];
+
+    tasks.push(taskInput);
+
     
+    console.log(tasks)
 }
