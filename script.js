c47184437ef2d7ca1fa01869ebb9d6c29e505925
ImranElOuafi <imran.el.ouafi@hva.nl>
2026-07-01 11:55:38 +0200

    refactor task management functions for improved clarity and consistency

--- script.js
+++ script.js
@@ -1,41 +1,23 @@
-let tasks;
+let tasks = [];
+
 
 const savedData = localStorage.getItem("taskList");
+const savedDataLabel = localStorage.getItem("taskLabel");
 
-if (savedData !== null){
-   tasks = JSON.parse(savedData);
-   console.log(tasks)
+if (savedData !== null) {
+  tasks = JSON.parse(savedData);
+  console.log(tasks)
 }
 
-else{
+else {
   tasks = [];
 }
 
-
 const taskDisplay = document.querySelector(".display-task");
 
-function manageTask() {
-  const inputElement = document.getElementById("task");
-  const taskInput = inputElement.value;
-  inputElement.value = "";
-  const taskButton = document.querySelector(".add-button");
 
-
-
-  // add task
-
+function displayTask() {
   taskDisplay.innerHTML = ``
-
-  taskDisplay.append();
-
-  if (taskInput === "" || taskInput === " ") {
-    alert("Can't add empty task");
-  }
-
-  else {
-    tasks.push(taskInput);
-    localStorage.setItem("taskList", JSON.stringify(tasks));
-  }
 
   tasks.forEach((task) => {
     taskDisplay.innerHTML += `
@@ -47,62 +29,91 @@ function manageTask() {
           </div>
         </div>
         `
-        
+
   })
 
   console.log(tasks)
-
-  //check task
-  taskDisplay.addEventListener("change", (taskChecked) => {
-
-    const checkBox = taskChecked.target
-    console.log(checkBox)
-
-    const taskLabelCheck = checkBox.nextElementSibling;
-
-    if (checkBox.classList.contains("check")) {
-      taskLabelCheck.style.textDecoration = "line-through";
-      taskLabelCheck.style.color = "rgba(19, 18, 18, 0.69)";
-      
-
-    }
-
-  })
 
   // delete task
   const deleteButtons = document.querySelectorAll(".delete-btn");
 
   deleteButtons.forEach((deleteButton) => {
     deleteButton.addEventListener("click", (event) => {
-      const deleteTask = event.target.parentNode;
-
-      const taskLabelDelete = deleteTask.querySelector(".task");
-
-      const taskTekst = taskLabelDelete.innerText.trim();
-      const index = tasks.indexOf(taskTekst);
-
-      if (index !== -1) {
-        tasks.splice(index, 1);
-        localStorage.setItem("taskList", JSON.stringify(tasks));
-        console.log("true");
-        
-      }
-
-      deleteTask.remove();
-
-      console.log("task deleted");
-      console.log(tasks);
+      const deleteElement = event.target.parentNode;
+      deleteTask(deleteElement);
+      
 
     })
   })
-
 }
 
 
 
+function addTask() {
+  const inputElement = document.getElementById("task");
+  const taskInput = inputElement.value;
+  inputElement.value = "";
+  const taskButton = document.querySelector(".add-button");
+
+  // add task
+
+  if (taskInput === "" || taskInput === " ") {
+    alert("Can't add empty task");
+  }
+
+  else {
+    tasks.push(taskInput);
+    localStorage.setItem("taskList", JSON.stringify(tasks));
+  }
+
+displayTask();
+}
 
 
+//check task
+  taskDisplay.addEventListener("change", (taskChecked) => {
 
+    const checkBox = taskChecked.target
+    checkTask(checkBox);
+  })
+
+function checkTask(checkBox) {
+  const taskLabelCheck = checkBox.nextElementSibling;
+
+  if (checkBox.classList.contains("check")) {
+    taskLabelCheck.style.textDecoration = "line-through";
+    taskLabelCheck.style.color = "rgba(19, 18, 18, 0.69)";
+    localStorage.setItem("taskLabel", JSON.stringify(taskLabelCheck));
+
+
+  }
+
+  console.log(checkBox)
+}
+
+
+function deleteTask(deleteElement) {
+  const taskLabelDelete = deleteElement.querySelector(".task");
+
+  const taskTekst = taskLabelDelete.innerText.trim();
+  const index = tasks.indexOf(taskTekst);
+
+  if (index !== -1) {
+    tasks.splice(index, 1);
+    localStorage.setItem("taskList", JSON.stringify(tasks));
+    console.log("true");
+
+  }
+
+  deleteElement.remove();
+
+  console.log("task deleted");
+  console.log(tasks);
+
+}
+
+
+displayTask();
 
 
 
